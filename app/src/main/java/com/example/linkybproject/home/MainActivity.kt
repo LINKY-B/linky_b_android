4916345150d7d95d1b3854b92399dcff2759cc02
Hajin74 <yoonmij9974@naver.com>
2023-01-09 01:02:10 +0900

    프로젝트 구조 세팅

--- app/src/main/java/com/example/linkybproject/home/MainActivity.kt
+++ app/src/main/java/com/example/linkybproject/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.linkybproject
+package com.example.linkybproject.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.linkybproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
