be7f34dc9dffcddd4adb31adb3e97b247dbf526a
hojeong2747 <hojeong2747@gmail.com>
2023-01-15 16:57:12 +0900

    add: activity_signup/2

---
+++ app/src/main/java/com/example/linkybproject/auth/SignupActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.linkybproject.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup2)
+    }
+}
