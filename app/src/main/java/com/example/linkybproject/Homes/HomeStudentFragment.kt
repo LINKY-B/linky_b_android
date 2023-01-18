0815e9bd5bf1c41c29148674394e86be2cc24cb8
jinjoo-jung <wjdwlswn23@naver.com>
2023-01-18 14:28:19 +0900

    jinjoo_push

---
+++ app/src/main/java/com/example/linkybproject/Homes/HomeStudentFragment.kt
@@ -0,0 +1,36 @@
+package com.example.linkybproject.Homes
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.linkybproject.R
+
+class HomeStudentFragment : Fragment() {
+    companion object {
+        fun newInstance() : HomeFragment  = HomeFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_students, container, false)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/linkybproject/Homes/PagerFragmentStateAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.linkybproject.Homes
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+private const val NUM_PAGES = 2
+
+class PagerFragmentStateAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = NUM_PAGES
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeStudentFragment()
+            1 -> HomeGraudateFragment()
+            else -> HomeStudentFragment()
+        }
+    }
+}
