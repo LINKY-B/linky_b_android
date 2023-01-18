bd717e00e891e018bdaf40b55b24ad029281d302
imi21123 <imi21123@naver.com>
2023-01-18 16:18:11 +0900

    dddd

---
+++ app/src/main/java/com/example/linkybproject/onBoarding/OnBoardingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.linkybproject.onBoarding
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.linkybproject.MainActivity
+import com.example.linkybproject.databinding.ActivityOnBoardingBinding
+
+class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityOnBoardingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }}
