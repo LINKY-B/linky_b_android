36e91756061cd04f42bcc17e4cb9243f0fbe6372
hojeong2747 <hojeong2747@gmail.com>
2023-01-19 20:48:16 +0900

    fix: activity_signup2.xml

--- app/src/main/java/com/example/linkybproject/auth/SignupActivity2.kt
+++ app/src/main/java/com/example/linkybproject/auth/SignupActivity2.kt
@@ -2,11 +2,54 @@ package com.example.linkybproject.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.linkybproject.R
+import com.example.linkybproject.databinding.ActivitySignup2Binding
 
 class SignupActivity2 : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignup2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup2)
+        binding = ActivitySignup2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 사용자 선택 항목으로 스피너 채우기
+        val spinnerYear: Spinner = binding.spinnerBirthYear
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.signup_birth_year,
+            R.layout.spinner_signup_item
+            //android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            adapter.setDropDownViewResource(R.layout.spinner_signup_dropdown)
+            spinnerYear.adapter = adapter
+        }
+
+        // 사용자 선택에 응답
+
+        val spinnerMonth: Spinner = binding.spinnerBirthMonth
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.signup_birth_month,
+            R.layout.spinner_signup_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_signup_dropdown)
+            spinnerMonth.adapter = adapter
+        }
+
+        val spinnerDay: Spinner = binding.spinnerBirthDay
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.signup_birth_day,
+            R.layout.spinner_signup_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_signup_dropdown)
+            spinnerDay.adapter = adapter
+        }
     }
 }
