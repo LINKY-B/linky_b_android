860152ab6ac3e997f588c3bc5bbe2b7e784b9246
imi21123 <imi21123@naver.com>
2023-02-15 07:48:42 +0900

    connectFromProfile detail

--- app/src/main/java/com/example/linkybproject/connect/ConnectFromProfileActivity.kt
+++ app/src/main/java/com/example/linkybproject/connect/ConnectFromProfileActivity.kt
@@ -3,6 +3,9 @@ package com.example.linkybproject.connect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.linkybproject.AcceptDialog
+import com.example.linkybproject.BlockDialog
+import com.example.linkybproject.ReportBlockDialog
+import com.example.linkybproject.ReportDialog
 import com.example.linkybproject.databinding.ActivityConnectFromProfileBinding
 
 class ConnectFromProfileActivity : AppCompatActivity() {
@@ -11,6 +14,52 @@ class ConnectFromProfileActivity : AppCompatActivity() {
         viewBinding = ActivityConnectFromProfileBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        viewBinding.btnConnectionProfileBack.setOnClickListener {
+            finish()
+        }
+
+        val name = intent.getStringExtra("name")
+        viewBinding.profileName.text = name
+
+        val like = intent.getIntExtra("like", 0)
+        viewBinding.profileLike.text = like.toString()
+
+        val numOfLink = intent.getIntExtra("numOfLink", 0)
+        viewBinding.numberOfLink.text = numOfLink.toString()
+
+        val major = intent.getStringExtra("major")
+        viewBinding.profileMajor.text = major
+
+        val classOf = intent.getIntExtra("classOf", 0)
+        viewBinding.profileClassOf.text = classOf.toString()
+
+        val age = intent.getIntExtra("age", 0)
+        viewBinding.profileAge.text = age.toString()
+
+        val gender = intent.getStringExtra("gender")
+        viewBinding.profileGender.text = gender
+
+        val mbti = intent.getStringExtra("mbti")
+        viewBinding.profileMbti.text = mbti
+
+        viewBinding.btnReportBlock.setOnClickListener {
+            val dlg = ReportBlockDialog(this)
+            dlg.Mydlg()
+
+            dlg.setOnClickedListener(object : ReportBlockDialog.ButtonClickListener {
+                override fun onClicked(myName: String) {
+                    if (myName == "report") {
+                        val dlgReport = ReportDialog(this@ConnectFromProfileActivity)
+                        dlgReport.Mydlg()
+                    }
+                    else if (myName == "block") {
+                        val dlgBlock = BlockDialog(this@ConnectFromProfileActivity)
+                        dlgBlock.Mydlg()
+                    }
+                }
+            })
+        }
 
         viewBinding.btnConnectionDelete.setOnClickListener {
             val dlg = DeleteDialog(this)
