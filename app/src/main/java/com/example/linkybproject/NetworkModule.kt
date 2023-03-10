2f0240175bf7dda779cce899c0b722ff32fbfa13
jinjoo-jung <wjdwlswn23@naver.com>
2023-03-10 16:58:22 +0900

    병합

--- app/src/main/java/com/example/linkybproject/NetworkModule.kt
+++ app/src/main/java/com/example/linkybproject/NetworkModule.kt
@@ -2,7 +2,9 @@ package com.example.linkybproject
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
