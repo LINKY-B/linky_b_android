8333b2d5caa851b0ee4009f5c1c9944ef07d6d5c
= <=>
2023-04-02 12:36:53 +0900

    Commit: commit

--- app/src/main/java/com/example/linkybproject/auth/LoginResponse.kt
+++ app/src/main/java/com/example/linkybproject/auth/LoginResponse.kt
@@ -3,8 +3,15 @@ package com.example.linkybproject.auth
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse (
-    @SerializedName(value = "grantType") val grantType: String,
-    @SerializedName(value = "accessToken") val accessToken: String,
-    @SerializedName(value = "refreshToken") val refreshToken: String,
-    @SerializedName(value = "accessTokenExpiresIn") val accessTokenExpiresIn: Long
+    @SerializedName(value = "status") val status: Int,
+    @SerializedName(value = "code") val code: String,
+    @SerializedName(value = "message") val message: String,
+    @SerializedName(value = "data") val data: String,
+    @SerializedName(value = "errors") val errors: List<LoginError>,
+)
+
+data class LoginError (
+    @SerializedName(value = "field") val field: String,
+    @SerializedName(value = "value") val value: String,
+    @SerializedName(value = "reason") val reason: String,
 )
--- app/src/main/java/com/example/linkybproject/auth/LoginService.kt
+++ app/src/main/java/com/example/linkybproject/auth/LoginService.kt
@@ -19,8 +19,12 @@ class LoginService {
         loginService.login(loginRequest).enqueue(object : Callback<LoginResponse> {
             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                 Log.d("Login(Success)", response.toString())
-                loginView.onLoginSuccess()
-                Log.d("Login(Success)", response.body()?.accessToken.toString())
+                val resp: LoginResponse = response.body()!!
+
+                when(resp.status) {
+                    200 -> loginView.onLoginSuccess()
+                    else -> loginView.onLoginFailure()
+                }
             }
 
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
